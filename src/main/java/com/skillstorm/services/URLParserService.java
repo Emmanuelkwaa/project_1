5dd20065a3efe694b949bc1008b56bbcedf40710
ONE BEAST <ONE BEAST@ONE-BEAST>
2022-07-22 11:25:21 -0400

    Documented URLParserService

--- src/main/java/com/skillstorm/services/URLParserService.java
+++ src/main/java/com/skillstorm/services/URLParserService.java
@@ -1,6 +1,15 @@
 package com.skillstorm.services;
 
+/**
+ * This class helps extract specific url endpoints from the request url
+ */
 public class URLParserService {
+
+    /**
+     * This method extracts ID from the giving url
+     * @param url   The giving url that will be dissected to extract ID
+     * @return int  Integer ID
+     */
     public int extractIdFromURL(String url) {
 //		System.out.println(url); // /12/123
         String[] splitString = url.split("/"); // [12, 123]
@@ -13,11 +22,16 @@ public class URLParserService {
         }
     }
 
-    public int extractQueryFromURL(String url) {
-		String splitString = url.substring(url.indexOf("=")+1);
-		return Integer.parseInt(splitString);
-	}
+//    public int extractQueryFromURL(String url) {
+//		String splitString = url.substring(url.indexOf("=")+1);
+//		return Integer.parseInt(splitString);
+//	}
 
+    /**
+     * This method extracts sub-url from the giving url
+     * @param url       The giving url that will be dissected to extract sub-url
+     * @return String   String sub-url
+     */
     public String extractSubUrlFromURL(String url) {
         try {
             String splitString = url.split("/")[1];
